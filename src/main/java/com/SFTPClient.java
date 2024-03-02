ca0e18b698d620782512d712c2706882940e984e
zubshaikk <zubairahmed.shaik@outlook.com>
2024-03-02 20:57:05 +0000

    Added SFTP Client functionality

--- src/main/java/com/SFTPClient.java
+++ src/main/java/com/SFTPClient.java
@@ -1,7 +1,89 @@
 package com;
 
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+
 public class SFTPClient {
+
+    private String host;
+    private int port;
+    private String username;
+    private String password;
+
+    public SFTPClient(String host, int port, String username, String password) {
+        this.host = host;
+        this.port = port;
+        this.username = username;
+        this.password = password;
+    }
+
+    public void uploadFile(String localFilePath, String remoteFilePath) {
+        Session session = null;
+        Channel channel = null;
+        ChannelSftp sftpChannel = null;
+        try {
+            JSch jsch = new JSch();
+            session = jsch.getSession(username, host, port);
+            session.setPassword(password);
+            session.setConfig("StrictHostKeyChecking", "no");
+            session.connect();
+
+            channel = session.openChannel("sftp");
+            channel.connect();
+            sftpChannel = (ChannelSftp) channel;
+
+            sftpChannel.put(localFilePath, remoteFilePath);
+            System.out.println("Upload successful.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (sftpChannel != null) {
+                sftpChannel.exit();
+            }
+            if (channel != null) {
+                channel.disconnect();
+            }
+            if (session != null) {
+                session.disconnect();
+            }
+        }
+    }
+
+    public void downloadFile(String remoteFilePath, String localFilePath) {
+        Session session = null;
+        Channel channel = null;
+        ChannelSftp sftpChannel = null;
+        try {
+            JSch jsch = new JSch();
+            session = jsch.getSession(username, host, port);
+            session.setPassword(password);
+            session.setConfig("StrictHostKeyChecking", "no");
+            session.connect();
+
+            channel = session.openChannel("sftp");
+            channel.connect();
+            sftpChannel = (ChannelSftp) channel;
+
+            sftpChannel.get(remoteFilePath, localFilePath);
+            System.out.println("Download successful.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (sftpChannel != null) {
+                sftpChannel.exit();
+            }
+            if (channel != null) {
+                channel.disconnect();
+            }
+            if (session != null) {
+                session.disconnect();
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
     }
 }
